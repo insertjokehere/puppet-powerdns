4613c8a3fad7119838bca2c45c4023f27067e49a
William Hughes <williamh@catalyst.net.nz>
2018-08-16 11:16:05 +1200

    Add defs for setting up tsig keys

---
+++ manifests/tsig_activate.pp
@@ -0,0 +1,13 @@
+define powerdns::tsig_activate($keyname=$name, $zone, $role) {
+
+  $meta = $role ? {
+    'master' => 'TSIG-ALLOW-AXFR',
+    'slave'  => 'AXFR-MASTER-TSIG',
+    default  => ''
+  }
+
+  exec { "activate-tsig-${keyname}-${zone}":
+    command => "/usr/bin/pdnsutil activate-tsig-key ${zone} ${keyname} ${role}",
+    unless  => "/usr/bin/pdsnutil get-meta ${zone} ${meta} | /bin/grep ${keyname}"
+  }
+}
